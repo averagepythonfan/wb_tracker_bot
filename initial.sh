7a0a20c71310f4b5f23704fbade556bd770f8acc
averagepythonfan <djonvorobei@bk.ru>
2023-02-24 14:20:21 +0300

    docker compose example added

---
+++ initial.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir pgdata
+
+docker pull postgres:alpine3.17
+
+docker pull instrumentisto/geckodriver
+
+docker pull python:3.10-slim
+
+docker build -f docker/parser/Dockerfile -t parser:final .
+
