bead6ff9d4e2bdda167cf88a57a88a2d3189878b
averagepythonfan <djonvorobei@bk.ru>
2023-04-27 14:43:57 +0300

    minor updates

--- bot/config.py
+++ bot/config.py
@@ -1,8 +1,11 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # tg_bot
 TOKEN: str = os.getenv('TOKEN')
-ADMIN_ID: int = int(os.getenv('ADMIN_ID'))
+ADMIN_ID: int = os.getenv('ADMIN_ID')
 COMMAND_EXECUTOR: str = os.getenv('COMMAND_EXECUTOR')
 ADMIN_USERNAME: str = os.getenv('ADMIN_USERNAME')
 
@@ -11,5 +14,5 @@ DRIVERNAME: str = os.getenv('DRIVERNAME')
 USERNAME: str = os.getenv('USERNAMEDB')
 PASSWORD: str = os.getenv('PASSWORD')
 HOST: str = os.getenv('HOST')
-PORT: int = int(os.getenv('PORT'))
+PORT: int = os.getenv('PORT')
 DATABASE: str = os.getenv('DATABASE')
